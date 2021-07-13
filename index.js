e1dd1c0ef21a0137a3fc1d5a83ed7635c60ad00e
Laura <lnkeat@gmail.com>
2021-07-12 18:22:05 -0600

    commit tree a0f10fe065bce9df680eb2785c6487b0325c8469

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function findWin(obj) {
+    return obj.result === 'W'
+  }
+  
+  function superbowlWin(arr){
+    if(arr.find(findWin) === undefined){
+      return undefined
+    }else {
+    return arr.find(findWin)['year']
+    }
+  }
